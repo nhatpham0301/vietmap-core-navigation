a0020511fa2795d04e2741b9edfd1aa1da720c03
dang trong thanh <thanhdt@vietmap.vn>
2023-03-21 16:12:25 +0700

    init project

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "VietMapGLNative",
+    products: [
+        .library(
+            name: "Mapbox",
+            targets: ["Mapbox"]),
+        .library(
+            name: "MapboxMobileEvents",
+            targets: ["MapboxMobileEvents"])  
+    ],
+    dependencies: [
+    ],    
+    targets: [
+        .binaryTarget(
+            name: "Mapbox",
+            url: "https://github.com/thanhdt1998/distribute-sdk/raw/main/Mapbox.xcframework.zip",
+            checksum: "d44dc655cbcdadb5d57d148730842cad7a3945641df1cecc349e887b04587543"),
+        .binaryTarget(
+            name: "MapboxMobileEvents",
+            url: "https://github.com/maptiler/maplibre-gl-native/releases/download/ios-v5.9.0/MapboxMobileEvents-5.9.0.zip",
+            checksum: "a96842857465adc2f2f3eeaa94dff25e58875bddd871e1a9edeef325591d111a")   
+            ]
+)
