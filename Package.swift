fd0a01a68b8ef712824a21f2f097ae76c2842683
nhatpv <nhatpv@vietmap.vn>
2023-04-17 17:05:27 +0700

    first commit

--- Package.swift
+++ Package.swift
@@ -2,25 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "Mapbox",
+    name: "VietmapCoreNavigaion",
     products: [
         .library(
-            name: "Mapbox",
-            targets: ["Mapbox"]),
-        .library(
-            name: "MapboxMobileEvents",
-            targets: ["MapboxMobileEvents"])  
+            name: "VietmapCoreNavigaion",
+            targets: ["VietmapCoreNavigaion"]))  
     ],
     dependencies: [
     ],    
     targets: [
         .binaryTarget(
-            name: "Mapbox",
-            url: "https://github.com/nhatpham0301/map-ios-sdk/raw/main/Mapbox.xcframework.zip",
-            checksum: "e87e06d11c4b5a73643061538c939c6c6190c7a3a818598135b34c19caa507c9"),
-        .binaryTarget(
-            name: "MapboxMobileEvents",
-            url: "https://github.com/maptiler/maplibre-gl-native/releases/download/ios-v5.9.0/MapboxMobileEvents-5.9.0.zip",
-            checksum: "a96842857465adc2f2f3eeaa94dff25e58875bddd871e1a9edeef325591d111a")   
+            name: "VietmapCoreNavigaion",
+            url: "https://github.com/nhatpham0301/vietmap-core-navigation/raw/main/VietmapCoreNavigaion.xcframework.zip",
+            checksum: "1faed175a701bcca9f05594e4cb60bd04b984364a218a9ee8525d2aba41710d5"),
             ]
 )
