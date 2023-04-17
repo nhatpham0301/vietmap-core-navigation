27cb77d45fd1cb1e3e571887cfce9c8975413921
nhatpv <nhatpv@vietmap.vn>
2023-04-17 17:14:31 +0700

    Release 1.0.0

--- Package.swift
+++ Package.swift
@@ -2,18 +2,18 @@
 import PackageDescription
 
 let package = Package(
-    name: "VietmapCoreNavigaion",
+    name: "MapboxCoreNavigation",
     products: [
         .library(
-            name: "VietmapCoreNavigaion",
-            targets: ["VietmapCoreNavigaion"]))  
+            name: "MapboxCoreNavigation",
+            targets: ["MapboxCoreNavigation"]))  
     ],
     dependencies: [
     ],    
     targets: [
         .binaryTarget(
-            name: "VietmapCoreNavigaion",
-            url: "https://github.com/nhatpham0301/vietmap-core-navigation/raw/main/VietmapCoreNavigaion.xcframework.zip",
+            name: "MapboxCoreNavigation",
+            url: "https://github.com/nhatpham0301/vietmap-core-navigation/raw/main/MapboxCoreNavigation.xcframework.zip",
             checksum: "1faed175a701bcca9f05594e4cb60bd04b984364a218a9ee8525d2aba41710d5"),
             ]
 )
