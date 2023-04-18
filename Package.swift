c924b244a537ba331f635c07c32a114218bab2f3
nhatpv <nhatpv@vietmap.vn>
2023-04-18 10:01:07 +0700

    Release 1.0.2

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     products: [
         .library(
             name: "MapboxCoreNavigation",
-            targets: ["MapboxCoreNavigation"]))  
+            targets: ["MapboxCoreNavigation"])
     ],
     dependencies: [
     ],    
